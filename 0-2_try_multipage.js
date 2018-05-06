6b05877edc93e64e1b96a400ad90dcde3c14bf2b
homoxapien <11745044+homoxapien@users.noreply.github.com>
2018-05-06 11:45:18 +0800

    Update 0-2_try_multipage.js
    semicolon added at the end of res.end(), makes no difference indeed

--- 0-2_try_multipage.js
+++ 0-2_try_multipage.js
@@ -12,7 +12,7 @@ var server = http.createServer(function (req, res){
 	}
 	else{
 		res.writeHead(404, {'Content-Type': 'text/html'});
-		res.end('Page Not Found')
+		res.end('Page Not Found');
 	}
   
 });
