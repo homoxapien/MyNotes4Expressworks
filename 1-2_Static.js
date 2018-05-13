011dd9738e947d9194b407e2cbd6bfbae8c1d41d
homoxapien <11745044+homoxapien@users.noreply.github.com>
2018-05-13 23:48:34 +0800

    Update 1-2_Static.js

--- 1-2_Static.js
+++ 1-2_Static.js
@@ -5,5 +5,6 @@ var app = express();
 //console.log(process.argv[3]);
 
 app.use(express.static(process.argv[3]));
+//express.static() is the function in express that serves a static file
 
 app.listen(process.argv[2]);
